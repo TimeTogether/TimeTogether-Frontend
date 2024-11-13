b7b8b7fd559c753c2ecdb5f64dbe33f176490b74
Bang Uhyeok <0105bang@gmail.com>
2024-11-13 18:33:52 +0900

    feat : groupPage upadate

--- src/pages/GroupPage.jsx
+++ src/pages/GroupPage.jsx
@@ -14,66 +14,68 @@ function GroupPage() {
   const [selectedGroup, setSelectedGroup] = useState(null); // 삭제할 그룹 정보
 
   // 백엔드에서 그룹 데이터를 가져오는 함수 (예시)
-  // useEffect(() => {
-  //   // 여기에 실제 API 호출 코드 작성
-  //   const fetchGroups = async () => {
-  //     try {
-  //       const response = await fetch("YOUR_API_ENDPOINT");
-  //       const data = await response.json();
-  //       setGroups(data); // 받아온 데이터를 상태에 저장
-  //     } catch (error) {
-  //       console.error("그룹 데이터를 가져오는 중 오류 발생:", error);
-  //     }
-  //   };
-
-  //   fetchGroups();
-  // }, []);
-  // 예시 데이터를 설정를 설정하는 함수.
-
   useEffect(() => {
-    // 새로운 배열 형식의 더미 응답 데이터
-    const exampleResponse = {
-      message: "요청에 성공했습니다.",
-      httpStatus: "OK",
-      data: [
-        {
-          groupId: 2,
-          groupName: "hello-world",
-          groupTitle: "헬로우 월드",
-          groupImg: "https://hello.com/helloworld/dy.jpg",
-          groupMembers: "101622476764454775716,100682045992698191363,123123132",
-          groupMgrId: "118042957275397174302",
-        },
-        {
-          groupId: 3,
-          groupName: "미야옹",
-          groupTitle: "미야옹 월드",
-          groupImg: "https://미야옹.com/helloworld/dy.jpg",
-          groupMembers: null,
-          groupMgrId: "100682045992698191363",
-        },
-        {
-          groupId: 4,
-          groupName: "미야옹",
-          groupTitle: "미야옹 월드",
-          groupImg: "https://미야옹.com/helloworld/dy.jpg",
-          groupMembers: "118042957275397174302,104544948724000293825",
-          groupMgrId: "100682045992698191363",
-        },
-        {
-          groupId: 5,
-          groupName: "강아지",
-          groupTitle: "강아지 월드",
-          groupImg: "https://강아지.com/helloworld/dy.jpg",
-          groupMembers: null,
-          groupMgrId: "100682045992698191363",
-        },
-      ],
+    // 여기에 실제 API 호출 코드 작성
+    const fetchGroups = async () => {
+      try {
+        n;
+        const response = await fetch(
+          "http://192.168.233.218:8080/group/groups/view"
+        );
+        const groupData = await response.json();
+        setGroups(groupData.data); // 받아온 데이터를 상태에 저장
+      } catch (error) {
+        console.error("그룹 데이터를 가져오는 중 오류 발생:", error);
+      }
     };
 
-    // 더미 데이터를 상태에 설정
-    setGroups(exampleResponse.data);
+    fetchGroups();
   }, []);
+
+  // useEffect(() => {
+  //   // 새로운 배열 형식의 더미 응답 데이터
+  //   const exampleResponse = {
+  //     message: "요청에 성공했습니다.",
+  //     httpStatus: "OK",
+  //     data: [
+  //       {
+  //         groupId: 2,
+  //         groupName: "hello-world",
+  //         groupTitle: "헬로우 월드",
+  //         groupImg: "https://hello.com/helloworld/dy.jpg",
+  //         groupMembers: "101622476764454775716,100682045992698191363,123123132",
+  //         groupMgrId: "118042957275397174302",
+  //       },
+  //       {
+  //         groupId: 3,
+  //         groupName: "미야옹",
+  //         groupTitle: "미야옹 월드",
+  //         groupImg: "https://미야옹.com/helloworld/dy.jpg",
+  //         groupMembers: null,
+  //         groupMgrId: "100682045992698191363",
+  //       },
+  //       {
+  //         groupId: 4,
+  //         groupName: "미야옹",
+  //         groupTitle: "미야옹 월드",
+  //         groupImg: "https://미야옹.com/helloworld/dy.jpg",
+  //         groupMembers: "118042957275397174302,104544948724000293825",
+  //         groupMgrId: "100682045992698191363",
+  //       },
+  //       {
+  //         groupId: 5,
+  //         groupName: "강아지",
+  //         groupTitle: "강아지 월드",
+  //         groupImg: "https://강아지.com/helloworld/dy.jpg",
+  //         groupMembers: null,
+  //         groupMgrId: "100682045992698191363",
+  //       },
+  //     ],
+  //   };
+
+  //   // 더미 데이터를 상태에 설정
+  //   setGroups(exampleResponse.data);
+  // }, []);
 
   const openInviteModal = () => setIsInviteModalOpen(true);
   const closeInviteModal = () => setIsInviteModalOpen(false);
